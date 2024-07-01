323f7672c3811e4c18af3714dee9535533bb8603
pivotiiii <17112987+pivotiiii@users.noreply.github.com>
2024-07-01 18:17:20 +0200

    update projects.js

--- js/projects.js
+++ js/projects.js
@@ -24,6 +24,22 @@ export const projects = new Array(
         desc_external_key: "description"
     },
     {
+        name: "Yu-Gi-Oh! Card Search Plugin for Flow Launcher", 
+        shortname: "FL Yu-Gi-Oh! Card Search", 
+        link: "https://github.com/pivotiiii/flow_launcher_ygo", 
+        description: "https://api.github.com/repos/pivotiiii/flow_launcher_ygo", 
+        desc_external: true,
+        desc_external_key: "description"
+    },
+    {
+        name: "Timer & Alarms Plugin for Flow Launcher", 
+        shortname: "FL Timer & Alarms", 
+        link: "https://github.com/pivotiiii/flow_launcher_timer", 
+        description: "https://api.github.com/repos/pivotiiii/flow_launcher_timer", 
+        desc_external: true,
+        desc_external_key: "description"
+    },
+    {
         name: "Wind Waker HD Playstation UI Mod", 
         shortname: "Wind Waker HD PS UI", 
         link: "https://gamebanana.com/mods/385841", 
