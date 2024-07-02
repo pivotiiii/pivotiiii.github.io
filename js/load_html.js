3e45bf64f8f32fe9c1730be9722a0f43c8fce92e
pivotiiii <17112987+pivotiiii@users.noreply.github.com>
2024-07-02 19:48:43 +0200

    remove await in top level

--- js/load_html.js
+++ js/load_html.js
@@ -1,18 +1,22 @@
 const divs = document.querySelectorAll(".replace");
-for (const div of divs) {
-    const file = await fetch(div.dataset.file);
-    const html = await file.text();
-    div.innerHTML = html;
+replaceDivs(divs);
 
-    if (div.dataset.style) {
-        div.style.cssText = div.dataset.style;
-    }
+async function replaceDivs(divs) {
+    for (const div of divs) {
+        const file = await fetch(div.dataset.file);
+        const html = await file.text();
+        div.innerHTML = html;
 
-    if (div.dataset.script) {
-        const script = document.createElement("script");
-        script.type = "module";
-        script.src = div.dataset.script;
-        //script.defer = true;
-        div.parentNode.appendChild(script);
+        if (div.dataset.style) {
+            div.style.cssText = div.dataset.style;
+        }
+
+        if (div.dataset.script) {
+            const script = document.createElement("script");
+            script.type = "module";
+            script.src = div.dataset.script;
+            //script.defer = true;
+            div.parentNode.appendChild(script);
+        }
     }
 }
