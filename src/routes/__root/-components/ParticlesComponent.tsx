02094f20ffb17929eec7dce597435c415de9e34a
pivotiiii <17112987+pivotiiii@users.noreply.github.com>
2025-02-25 01:13:58 +0100

    remove unneccessary ids

--- src/routes/__root/-components/ParticlesComponent.tsx
+++ src/routes/__root/-components/ParticlesComponent.tsx
@@ -2,7 +2,7 @@ import "./ParticlesComponent.css";
 
 export function ParticlesComponent(props: {color: string}) {
     return (
-        <div id="particlesContainer" className={"particlesContainer " + props.color}>
+        <div className={"particlesContainer " + props.color}>
             {Array.from(Array(30), (_, idx) => (
                 <div key={"particle" + idx} className="particle"></div>
             ))}
