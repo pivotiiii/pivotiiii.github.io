be35891a149bf989d9ddc656b5e6a72f35cdcd16
pivotiiii <17112987+pivotiiii@users.noreply.github.com>
2025-03-02 00:56:43 +0100

    remove purgecss, link picocss from cdn

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react";
 import {TanStackRouterVite} from "@tanstack/router-plugin/vite";
 import {imagetools} from "vite-imagetools";
-import pluginPurgeCss from "vite-plugin-purgecss-updated-v5";
 
 const ReactCompilerConfig = {
     target: "19",
@@ -13,7 +12,6 @@ export default defineConfig({
     define: {__URL__: JSON.stringify("https://pivotiiii.pages.dev")},
     plugins: [
         imagetools(),
-        pluginPurgeCss(),
         TanStackRouterVite({autoCodeSplitting: true}),
         react({babel: {plugins: [["babel-plugin-react-compiler", ReactCompilerConfig]]}}),
     ],
