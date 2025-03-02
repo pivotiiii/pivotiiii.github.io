d469f8c3c2aab0de1131523faeed51fe4ce394e0
pivotiiii <17112987+pivotiiii@users.noreply.github.com>
2025-03-02 02:55:09 +0100

    update submodule

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,5 +1,13 @@
 declare module "*.webp";
 declare module "*.png";
+declare module "*&as=metadata&imagetools" {
+    const outputs: {
+        width: string;
+        height: string;
+        src: string;
+    };
+    export default outputs;
+}
 declare module "*&imagetools" {
     const outputs: string;
     export default outputs;
